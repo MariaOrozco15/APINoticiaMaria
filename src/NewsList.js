41977e67fa62db98f9c0f3af2b6a30fffdfe0a5f
MariaOrozco15 <mo6569496@gmail.com>
2023-06-15 16:43:48 -0600

    Defina el componente NewsList como una funcion que devuelve la interfaz de usuario de la lista de noticias

--- src/NewsList.js
+++ src/NewsList.js
@@ -1,3 +1,47 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, FlatList, TouchableOpacity, Stylesheet} from 'react-native';
 import axios from 'axios';
+
+//Defina el componente NewsList como una funcion que devuelve la interfaz de usuario de la lista
+//de noticias
+
+const NewsList = ({navigation}) => { //declaramos una funcion(componente)
+    const [news, setNews] = useState([]); //crear un arreglo vacio con una variable de estado
+
+    useEffect(() => {
+        fetchNews();
+    }, []);
+
+    const fetchNews = async () => {
+        try {
+            const response = await axios.get('https://newsapi.org/v2/top.headlines', {
+                params: {
+                    country: 'us', //código del pais deseado
+                    apiKey: '44de9909569e45b1ba9b0b73b007ca83' //código de la API
+                },
+            });
+            setNews(response.data.articles);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return(
+        <View style = {styles.container}>
+            <FlatList
+                data = {news}
+                renderItems = {({ item }) =>(
+                    <TouchableOpacity style= {styles.newsItem}
+                    Key = {item.id}
+                    onPress = {() => navigation.navigate('NewsDetail', {newsItem: item})}
+                    >
+                        <Text style = {styles.title}>{item.title}</Text>
+                        <Text style= {styles.description}>{item.description}</Text>
+
+                    </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.id?.toString()}
+            />
+        </View>
+    );
+};
